2e424381daed666b6362300964db154e628caa71
Ankit Anand <ankitanand@Ankits-MacBook-Air.local>
2025-06-22 14:54:06 +0530

    middle section for homepage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,7 @@
 import Navbar from './Navbar';
 import '../styles/Banner.css';
+import Card from './Card';
+import { IMAGE_URLS } from '../utils/constants';
 
 function Banner() {
     return (
@@ -15,6 +17,63 @@ function Banner() {
                         <button className="hero-signin-btn">Sign In</button>
                     </div>
                 </div>
+
+                <section className="second-section">
+                    <div className="left-panel">
+                        <h2 className="city-tales-title">City Tales</h2>
+                        <p className="city-tales-desc">
+                            Join us on an illuminating exploration. Delve into a
+                            world of heartfelt recollections, hidden gems, and
+                            become part of an ever-growing storytelling
+                            community. Because every city has tales waiting to
+                            be told.
+                        </p>
+                        <input
+                            type="text"
+                            className="story-input"
+                            placeholder="Name your adventure..."
+                        />
+                        <button className="start-story-btn">Start Story</button>
+                    </div>
+
+                    <div className="right-cards">
+                        <Card
+                            title="Your City"
+                            imageSrc={IMAGE_URLS.CITY}
+                            imageAlt="Home"
+                            description="Discover real experiences around your city"
+                            buttonText="Your city"
+                            // onButtonClick={() => handleYourCityClick()}
+                        />
+
+                        <Card
+                            title="Top Storytellers"
+                            imageSrc={IMAGE_URLS.TRENDING}
+                            imageAlt="Trending"
+                            description="Meet the best storytellers"
+                            buttonText="Top Storytellers"
+                            // onButtonClick={() => handleStorytellerClick()}
+                        />
+
+                        <Card
+                            title="Category"
+                            imageSrc={IMAGE_URLS.CATEGORY}
+                            imageAlt="Categories"
+                            description="Explore categories from travel to lifestyle"
+                            buttonText="Category"
+                            // onButtonClick={() => handleCategoryClick()}
+                        />
+
+                        <Card
+                            title="Write Your Story"
+                            imageSrc={IMAGE_URLS.WRITE_YOUR_STORY}
+                            imageAlt="Sign In"
+                            description="Connect with people with your journey."
+                            buttonText="Write your story"
+                            // onButtonClick={() => handleWriteStoryClick()}
+                        />
+                    </div>
+                </section>
             </header>
         </>
     );
